cf7d894fa1d895b4d0d43b97d177ac5e626cf599
Suhaan995 <sp0016@srmist.edu.in>
2026-02-23 21:59:08 +0530

    Add files via upload

---
+++ OOPSBANNERuc4.java
@@ -0,0 +1,13 @@
+public class OOPSBANNERuc4 
+{
+    public static void main(String[] args){
+     System.out.println("   *        *     **   **");
+     System.out.println(" *   *    *   *   *   *  *  *");
+     System.out.println("*     *  *     *  *   *  **    ");
+     System.out.println("*     *  *     *  **   **");
+     System.out.println("*     *  *     *  *           *");
+     System.out.println(" *   *    *   *   *       *  **");
+     System.out.println("   *        *     *       ***");
+    } 
+    
+}
